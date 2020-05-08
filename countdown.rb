3c6fd2979eb3be2f6602d494cc4157097d10b5fb
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-08 03:14:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,6 +8,9 @@ def countdown(number)
 end
 
 def countdown_with_sleep(number)
-  sleep 2
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  sleep 1
   return "HAPPY NEW YEAR!"
 end
