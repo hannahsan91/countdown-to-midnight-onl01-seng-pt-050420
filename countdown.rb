5614e7d04a9726a088e65da46387868414d3d3d4
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-08 03:06:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
-#write your code here
+time = 10
+  while time < 11
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
 
-def countdown
+def countdown_with_sleep
+  sleep 1.second until time - 10.seconds
+  #break when true
 end
